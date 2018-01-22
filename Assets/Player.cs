a97c77de7482c75aeccafee4a1aa3ce1de6083b2
Hayri Durmaz <hayri.durmaz@tedu.edu.tr>
2018-01-22 12:05:48 +0300

    touch things added

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -13,7 +13,30 @@ public class Player : MonoBehaviour {
     }
     private void FixedUpdate()
     {
-        float x = Input.GetAxis("Horizontal") * Time.fixedDeltaTime * speed;
+        float k = 0;
+        for (int i = 0; i < Input.touchCount; i++)
+        {
+            Touch touch = Input.GetTouch(i);
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                Debug.Log(touch.position.x);
+                if (touch.position.x > (Screen.width / 2))
+                {
+                    k = 15f;//moves player right
+                }
+
+                if (touch.position.x < (Screen.width / 2))
+                {
+                    k= -15f; //moves player left
+                }
+            }
+        }
+
+        //Debug.Log(Input.GetAxis("Horizontal"));
+
+        //Input.GetAxis("Horizontal")
+        float x =k* Time.fixedDeltaTime * speed;
         Vector2 newPosition = rb.position + Vector2.right * x;
         newPosition.x = Mathf.Clamp(newPosition.x, -mapWidth, mapWidth);
         rb.MovePosition(newPosition);
